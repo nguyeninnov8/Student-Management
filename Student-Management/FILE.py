825c6d3f741b60936793bb9fe15a4fe7ff852e19
Qorilz89._ <90020801+LinnZ89@users.noreply.github.com>
2024-05-05 17:21:05 +0700

    refix Write and Read file function

--- Student-Management/FILE.py
+++ Student-Management/FILE.py
@@ -1,5 +1,6 @@
 import A_R_WAREHOUSE
 import WAREHOUSE_CLASS
+
 def write_to_file():
     with open("Warehouses.txt", "w") as f:
         for warehouse in A_R_WAREHOUSE.warehouses_list:
@@ -7,15 +8,31 @@ def write_to_file():
             for dest_warehouse, cost in warehouse.transportation_costs.items():
                 f.write(f"{dest_warehouse},{cost}\n")
 
+            f.write("\n")
+
+def delete_from_file():
+    with open("Warehouses.txt", "w") as f:
+        f.truncate(0)
+
+def update_into_file():
+    delete_from_file()
+    write_to_file()
+
 def read_from_file():
     with open("Warehouses.txt", "r") as f:
         lines = f.readlines()
-        for i in range(0, len(lines), 2):
+        i = 0
+        while i < len(lines):
             name, location, capacity = lines[i].strip().split(",")
             new_warehouse = WAREHOUSE_CLASS.Warehouse(name, location, int(capacity))
             A_R_WAREHOUSE.warehouses_list.append(new_warehouse)
-            for dest_warehouse, cost in lines[i + 1].strip().split(","):
-                new_warehouse.add_transportation_cost(dest_warehouse, int(cost))
+            i += 1
+            while i < len(lines) and lines[i].strip():
+                if len(lines[i].strip().split(",")) == 2:
+                    dest_warehouse, cost = lines[i].strip().split(",")
+                    new_warehouse.add_transportation_cost(dest_warehouse, int(cost))
+                else:
+                    print(f"Ignoring line {i + 1} due to invalid format")
+                i += 1  
+            i += 1
 
-def delete_from_file():
-    open("Warehouses.txt", "w").close()
